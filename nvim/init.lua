c16a3b8c5115cf9416da8d5f2d594fda0204a6e8
Nadelio <carefulsniffle@gmail.com>
2025-09-29 21:26:13 -0500

    Updating dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 -- various nvim editor settings
-vim.cmd([[set mouse=]])
+vim.opt.spell = true
 vim.opt.winborder = "rounded"
 vim.opt.hlsearch = false
 vim.opt.tabstop = 2
@@ -17,7 +17,10 @@ vim.opt.undofile = true
 vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 vim.opt.encoding = "utf-8"
+vim.opt.fileformat = "unix"
 vim.o.showmode = false
+vim.opt.listchars = { tab = "| " }
+vim.opt.list = true
 
 vim.g.rustfmt_autosave = 1
 vim.g.rustfmt_emit_files = 1
@@ -36,8 +39,12 @@ Plug('https://github.com/MeanderingProgrammer/render-markdown.nvim.git')
 Plug('https://github.com/typicode/bg.nvim.git')
 Plug('https://github.com/m4xshen/autoclose.nvim.git')
 Plug('daltonmenezes/aura-theme', {['rtp'] = 'packages/neovim'})
+Plug('datsfilipe/gruvbox.nvim')
 Plug('rust-lang/rust.vim')
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ":TSUpdate"})
+Plug('https://github.com/chomosuke/typst-preview.nvim')
+Plug('https://github.com/ravibrock/spellwarn.nvim')
+Plug('rktjmp/playtime.nvim')
 
 vim.call('plug#end')
 
@@ -63,20 +70,45 @@ vim.lsp.config["rust_analyzer"] = {
 		}
 	},
 	cmd = { "rust-analyzer" },
-  filetypes = { "rust" },
-  single_file_support = true,
-  root_markers = { ".git", "Cargo.toml" },
-  settings = {
-    ["rust-analyzer"] = {
-      check = { command = "clippy", extraArgs = { "--tests" } },
-      cargo = { features = "all" },
-      rustfmt = { extraArgs = { "+nightly" } },
-      inlayHints = { chainingHints = { enable = false } },
-    },
-  },
+	filetypes = { "rust" },
+	single_file_support = true,
+	root_markers = { ".git", "Cargo.toml" },
+	settings = {
+		["rust-analyzer"] = {
+			check = { command = "clippy", extraArgs = { "--tests" } },
+			cargo = { features = "all" },
+			rustfmt = { extraArgs = { "+nightly" } },
+			inlayHints = { chainingHints = { enable = false } },
+		},
+	},
 }
 
 vim.lsp.enable("rust_analyzer")
+
+-- Typst/TinyMist settings
+vim.lsp.config["tinymist"] = {
+	cmd = { "tinymist" },
+	filetypes = { "typst" },
+	settings = {}
+}
+
+vim.lsp.enable("tinymist")
+
+vim.cmd([[
+	augroup typst_entry_settings
+		autocmd!
+		autocmd FileType typst setlocal spell spelllang=en_us
+	augroup END
+]])
+
+-- Markdown settings
+vim.cmd([[
+	augroup markdown_entry_settings
+		autocmd!
+		autocmd FileType markdown setlocal spell spelllang=en_us
+	augroup END
+]])
+
 
 -- Lualine settings
 local theme = require('lualine.themes.horizon')
@@ -86,6 +118,9 @@ theme.visual.a.bg = '#ffca85'
 theme.replace.a.bg = '#a277ff'
 theme.command.a.bg = '#61ffca'
 theme.inactive.a.bg = '#f694ff'
+
+-- Gruvbox Dark theme for lualine
+-- local theme = require('lualine.themes.gruvbox_dark')
 
 -- General LSP settings
 if vim.lsp.inlay_hint then
@@ -105,20 +140,61 @@ end
 vim.keymap.set('n', "<Leader>e", function() vim.diagnostic.open_float({ focus = false }) end, {})
 vim.keymap.set('n', "<Leader>j", function() vim.diagnostic.goto_next() end)
 vim.keymap.set('n', "<Leader>k", function() vim.diagnostic.goto_prev() end)
-vim.keymap.set('n', "<Leader>s", ':w<CR>')
-vim.keymap.set('i', "<C-e>", '<Esc>', { noremap = true, silent = true })
 vim.keymap.set('n', "<Leader>i", function() vim.lsp.buf.hover({ focus = false }) end)
 
+-- General Neovim binds
+vim.keymap.set('i', "<C-e>", '<Esc>', { noremap = true, silent = true }) -- Move to Normal mode
+vim.keymap.set('i', "<C-s>", '<Esc>:w<CR>i<Right>', { noremap = true, silent = true }) -- save in Insert mode
+vim.keymap.set('i', "<C-z>", '<Esc>ui', { noremap = true, silent = true }) -- Undo in Insert mode
+vim.keymap.set('i', "<C-y>", '<Esc>:redo<CR>i', { noremap = true, silent = true}) -- Redo in insert mode
+vim.keymap.set('n', "<C-e>", 'i<Right>', { noremap = false, silent = true }) -- Move to Insert mode
+
 -- Neo-tree binds
-vim.keymap.set('n', "<Leader>f", ":Neotree<CR>", { silent = true })
+vim.keymap.set('n', "<Leader>f", ":Neotree<CR>", { silent = true }) -- Open file browser window
 
 -- requires
+require('gruvbox').setup({ transparent = true })
 require('lualine').setup({
 	options = {
 		theme = theme
 	}
 })
 require('render-markdown').setup()
+require('spellwarn').setup({
+	event = {
+		"CursorHold",
+		"InsertLeave",
+		"TextChanged",
+		"TextChangedI",
+		"TextChangedP"
+	},
+	enable = true,
+	ft_config = {
+		alpha = false,
+		help = false,
+		-- iter = false,
+		lazy = false,
+		lspinfo = false,
+		mason = false
+		-- look into docs for this
+	},
+	ft_default = true,
+	max_file_size = nil,
+	severity = {
+		spellbad = "WARN",
+		spellcap = "HINT",
+		spelllocal = "HINT",
+		spellrare = "INFO"
+	},
+	suggest = true,
+	num_suggest = 3,
+	prefix = "Possible issue(s): ",
+	diagnostic_opts = { severity_sort = true }
+})
+require('spellwarn').enable()
+require('playtime').setup({
+	window_border = "rounded"
+})
 require('autoclose').setup()
 require('neo-tree').setup({})
 require('nvim-treesitter.install').prefer_git = false
@@ -131,7 +207,8 @@ require('nvim-treesitter.configs').setup {
 	},
 }
 
--- bg and color theme
+-- background and color theme
 vim.cmd('silent! colorscheme aura-dark')
+-- vim.cmd.colorscheme('gruvbox') -- gruvbox dark theme
 vim.cmd.highlight({ "Normal", "guibg=NONE" })
 vim.cmd.highlight({ "Normal", "ctermbg=NONE" })
